62cb37d46ef12de61b90dcdc4a94ce5807cb8376
janper231 <jeancarloss659@gmail.com>
2022-07-04 17:40:26 -0500

    fix: indentation

--- pages/_app.js
+++ pages/_app.js
@@ -3,23 +3,23 @@ import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <title>Challenge</title>
-      </Head>
-      <>
-        <Component {...pageProps} />
-      </>
-      <footer>
-        <h5>
-          <a href="https://github.com/janper231" target="_blank">
-            By @janper231
-          </a>
-        </h5>
-      </footer>
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<title>Challenge</title>
+			</Head>
+			<>
+				<Component {...pageProps} />
+			</>
+			<footer>
+				<h5>
+					<a href="https://github.com/janper231" target="_blank">
+						By @janper231
+					</a>
+				</h5>
+			</footer>
+		</>
+	);
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Users from '../components/views/users';
 
 const Index = () => {
-  return <Users />;
+	return <Users />;
 };
 
 export default Index;
